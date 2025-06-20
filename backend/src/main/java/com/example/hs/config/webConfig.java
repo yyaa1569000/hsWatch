a3c387506f4678e61646f96baf1ea7c30260533e
dragon <dragon@wethink.com.tw>
2025-06-21 00:26:30 +0800

    update image

--- backend/src/main/java/com/example/hs/config/webConfig.java
+++ backend/src/main/java/com/example/hs/config/webConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class webConfig {
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
