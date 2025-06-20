68c758876b45e61b5e75b0a89a68fc532c4b3232
dragon <dragon@wethink.com.tw>
2025-06-20 23:13:35 +0800

    updata CORSCConfigurer

---
+++ backend/src/main/java/com/example/hs/config/webConfig.java
@@ -0,0 +1,23 @@
+package com.example.hs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://yyaa1569000.github.io") // 只允許你的前端
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
