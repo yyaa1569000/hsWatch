69e47abd68e7a28ab8dc4981bbce19fa332792d1
dragon <dragon@wethink.com.tw>
2025-06-21 01:23:18 +0800

    修改 HsItem

--- backend/src/main/java/com/example/hs/entity/HsItem.java
+++ backend/src/main/java/com/example/hs/entity/HsItem.java
@@ -24,7 +24,7 @@ public class HsItem {
     private String remark;
     private BigDecimal price;
 
-    @Column(name = "created_at", insertable = false, updatable = false)
+    @Column(name = "created_at")
     private Timestamp createdAt;
 
     // Getter and Setter
