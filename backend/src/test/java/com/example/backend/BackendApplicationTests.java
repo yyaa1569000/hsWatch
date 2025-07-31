a12ef1764894e18bea381c5e26fd7af7fa0b0a31
yyyaa1569000 <yyaa1569@gmail.com>
2025-07-31 12:07:27 +0800

    backend up

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.hs.HsApplication;
+
+@SpringBootTest(classes = HsApplication.class)
 class BackendApplicationTests {
 
 	@Test
